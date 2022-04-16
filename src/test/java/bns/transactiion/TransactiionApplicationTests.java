60128475c7a6b94a81aa81edd830fe374acf14be
firaslaroussi98 <firas.laroussi@esprit.tn>
2022-04-16 19:15:27 +0200

    First Commit

---
+++ src/test/java/bns/transactiion/TransactiionApplicationTests.java
@@ -0,0 +1,13 @@
+package bns.transactiion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactiionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
